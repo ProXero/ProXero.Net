6d468a10e08dc3e6d36bc4c2057c36f7b2e8c77f
Csabi <b.csabor@gmail.com>
2015-12-15 20:04:57 +0100

    changed nuget packages to released versions

--- ProXero.Net/Properties/AssemblyInfo.cs
+++ ProXero.Net/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ProXero.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple server-client communication library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ProXero")]
 [assembly: AssemblyProduct("ProXero.Net")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
