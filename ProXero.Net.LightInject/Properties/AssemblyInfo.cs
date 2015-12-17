1d1c5488e1fd17517cda9d121b7a34d04201cb2e
Csabi <b.csabor@gmail.com>
2015-12-17 20:52:55 +0100

    filled author and description

--- ProXero.Net.LightInject/Properties/AssemblyInfo.cs
+++ ProXero.Net.LightInject/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ProXero.Net.LightInject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("LightInject module for ProXero.Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ProXero")]
 [assembly: AssemblyProduct("ProXero.Net.LightInject")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
